e3fcc54f63245ee2ca1c1f957c8931b2e9407e3d
mboeddeker <marvin.boeddeker@gmail.com>
2016-11-16 00:49:42 +0100

    First Commit. Only Start is ready

---
+++ app/src/main/java/com/buchzulang/kurrency/Activities/MainActivity.java
@@ -0,0 +1,46 @@
+package com.buchzulang.kurrency.Activities;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.buchzulang.kurrency.Fragments.StartFragment;
+import com.buchzulang.kurrency.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    //  =   =   =   =   =   =   =   =   =   =   =   =
+    public static MainActivity mainActivity;
+
+    public static MainActivity getMainActivity() {
+        return mainActivity;
+    }
+
+    public static void setMainActivity(MainActivity mainActivity) {
+        MainActivity.mainActivity = mainActivity;
+    }
+
+    //  =   =   =   =   =   =   =   =   =   =   =   =
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MainActivity.setMainActivity(this);
+
+        FragmentManager fm = getSupportFragmentManager();
+        StartFragment startFragment = (StartFragment) fm.findFragmentById(R.id.container_main);
+        if (startFragment == null){
+            startFragment = StartFragment.newInstance("","");
+            fm.beginTransaction().add(R.id.container_main,startFragment).commit();
+        }
+
+    }
+
+    public void loadOperationScreen(){
+        Toast.makeText(mainActivity, "Hello", Toast.LENGTH_SHORT).show();
+        //getSupportFragmentManager().beginTransaction().replace(R.id.container_main,)
+    }
+}
---
+++ app/src/main/java/com/buchzulang/kurrency/Fragments/StartFragment.java
@@ -0,0 +1,60 @@
+package com.buchzulang.kurrency.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.buchzulang.kurrency.Activities.MainActivity;
+import com.buchzulang.kurrency.R;
+
+public class StartFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+
+    public StartFragment() {
+
+    }
+
+    public static StartFragment newInstance(String param1, String param2) {
+        StartFragment fragment = new StartFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_start, container, false);
+
+        Button btn_Start = (Button) v.findViewById(R.id.btn_start);
+        btn_Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.getMainActivity().loadOperationScreen();
+            }
+        });
+        
+        return v;
+    }
+
+}
